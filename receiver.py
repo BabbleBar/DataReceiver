2bfea67a25ecf25851b476f2da32f9e157f80cc4
Florian Schrag <florian@schrag.org>
2015-11-07 18:57:19 +0100

    try using data

--- receiver.py
+++ receiver.py
@@ -3,6 +3,7 @@ import os
 
 import pika
 import xmltodict
+from dateutil.parser import parse
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -22,6 +23,29 @@ def get_pika_params():
     return pika.ConnectionParameters(host="localhost")
 
 
+def parse_value(data, data_type):
+    return {
+        'eui': data['DevEUI'],
+        'data_type': data_type,
+        'timestamp': parse(data['Time']),
+        'payload_hex': data['payload_hex'],
+        'payload_int': int(data['payload_hex'], 16),
+        'payload_float': float(data['payload_hex'], 16),
+        'lat': float(data['LrrLAT']),
+        'lon': float(data['LrrLON']),
+    }
+
+
+def send_value(data, data_type):
+    dict_data = parse_value(data, data_type)
+    json_data = json.dumps(dict_data, indent=4, separators=(',', ': '))
+    print("SendVALUE: ")
+    print(json_data)
+    channel_data.basic_publish(exchange='data',
+                               routing_key='',
+                               body=json_data)
+
+
 @app.route("/cb", methods=["POST"])
 def cb():
     LrnDevEui = request.args.get('LrnDevEui', '')
@@ -34,9 +58,12 @@ def cb():
     json_data = json.dumps(dict_data['DevEUI_uplink'], indent=4, separators=(',', ': '))
     print(json_data)
 
-    channel.basic_publish(exchange='data_log',
-                          routing_key='',
-                          body=json_data)
+    channel_log.basic_publish(exchange='data_log',
+                              routing_key='',
+                              body=json_data)
+
+    if dict_data['FPort'] == '3':
+        send_value(dict_data, 'lum')
 
     return "processed"
 
@@ -45,6 +72,8 @@ if __name__ == "__main__":
     port = os.getenv('VCAP_APP_PORT', '5000')
 
     connection = pika.BlockingConnection(get_pika_params())
-    channel = connection.channel()
-    channel.exchange_declare(exchange='data_log', type='fanout')
+    channel_log = connection.channel()
+    channel_log.exchange_declare(exchange='data_log', type='fanout')
+    channel_data = connection.channel()
+    channel_data.exchange_declare(exchange='data', type='fanout')
     app.run(host='0.0.0.0', port=int(port))
